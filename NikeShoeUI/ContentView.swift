5fb78313b4724c6d5282041670b94b45c77ab886
rick2785 <rjhrabowskie@gmail.com>
2020-09-13 02:13:06 -0400

    Initial Commit

---
+++ NikeShoeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NikeShoeUI
+//
+//  Created by RJ Hrabowskie on 9/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NikeShoeUI/NikeShoeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeShoeUIApp.swift
+//  NikeShoeUI
+//
+//  Created by RJ Hrabowskie on 9/13/20.
+//
+
+import SwiftUI
+
+@main
+struct NikeShoeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
